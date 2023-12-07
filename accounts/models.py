08dfcc29961ccbe86c5e70431a849541b15fa996
Nishant825 <rnishant2796@gmail.com>
2023-12-07 18:51:52 +0530

    price field added

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Address(models.Model):
+    address = models.CharField(max_length=255)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    postal_code = models.CharField(max_length=20)
+    country = models.CharField(max_length=100)
+    phone_no = models.CharField(max_length=15)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_default = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.address}, {self.city}, {self.state} {self.postal_code}, {self.country}"
--- libraryapp/models.py
+++ libraryapp/models.py
@@ -2,38 +2,42 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.cache import cache
 
+
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
-    last_name  = models.CharField(max_length=100)
-    
-    def __str__(self):
-        return self.first_name +" "+self.last_name
+    last_name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.first_name + " " + self.last_name
 
 
 class Genre(models.Model):
     name = models.CharField(max_length=50)
-    
-    def __str__(self): 
+
+    def __str__(self):
         return self.name
 
 
 STOCK_CHOICES = (
-        ('yes', 'Yes'),
-        ('no', 'No'),
-    )
+    ("yes", "Yes"),
+    ("no", "No"),
+)
+
 
 class Book(models.Model):
-    cover_img = models.ImageField(upload_to='images')
+    cover_img = models.ImageField(upload_to="images")
     title = models.CharField(max_length=100)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     genre = models.ForeignKey(Genre, on_delete=models.CASCADE, null=True)
-    availability_status = models.CharField(max_length=3, choices=STOCK_CHOICES, default='yes')
- 
+    price = models.PositiveIntegerField(default=0)
+    availability_status = models.CharField(
+        max_length=3, choices=STOCK_CHOICES, default="yes"
+    )
+
     def save(self, *args, **kwargs):
-        cache.delete('books')
+        cache.delete("books")
         super(Book, self).save(*args, **kwargs)
-    
+
     def __str__(self):
         return self.title
 
@@ -42,10 +46,10 @@ class BookBorrow(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     due_date = models.DateField(null=True)
-    
+
     class Meta:
-       ordering = ['-id']
-    
+        ordering = ["-id"]
+
     def format_name(self):
         return f"{self.user.first_name.capitalize()} {self.user.last_name.capitalize()}"
 
@@ -62,16 +66,16 @@ class BookBorrowHistory(models.Model):
     paid = models.BooleanField(default=False)
 
     class Meta:
-       ordering = ['-id']
-    
+        ordering = ["-id"]
+
     def format_name(self):
         return f"{self.user.first_name.capitalize()} {self.user.last_name.capitalize()}"
 
     def calculate_fine(self):
         charges = (self.due_date - self.return_date).days
         if self.due_date:
-            if charges<0:
-                charges = abs(charges)*20
+            if charges < 0:
+                charges = abs(charges) * 20
             else:
                 charges = 0
             return charges
